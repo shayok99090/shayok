aa68314592e38217e19440ef3add6e99b7b34825
shayok99090 <122869889+shayok99090@users.noreply.github.com>
2023-10-04 14:06:10 +0600

    Update scripts2.sh

--- scripts2.sh
+++ scripts2.sh
@@ -1,1 +1,10 @@
-
+version: 0.0
+os: linux
+files:
+  - source: /
+    destination: /var/www/myapp
+hooks:
+  - BeforeInstall:
+      - location: scripts.sh
+        timeout: 600
+        runas: ec2-user
